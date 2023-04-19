03880bb624246a766854899a2b9ed5b33eca168c
theia <theia@mit.edu>
2023-04-19 11:03:06 -0400

    Chat lab

---
+++ chat.js
@@ -0,0 +1,179 @@
+import * as Vue from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import GraffitiPlugin from '../graffiti-js/plugins/vue/plugin.js'
+
+const app = {
+  setup() {
+    // Initialize the name of the channel we're chatting in
+    const channel = Vue.ref('default')
+
+    // And a flag for whether or not we're private-messaging
+    const privateMessaging = Vue.ref(false)
+
+    // If we're private messaging use "me" as the channel,
+    // otherwise use the value
+    const $gf = Vue.inject('graffiti')
+    const context = Vue.computed(()=> privateMessaging.value? [$gf.me] : [channel.value])
+
+    // Initialize the collection of messages associated with the context
+    const { objects: messagesRaw } = $gf.useObjects(context)
+    return { channel, privateMessaging, messagesRaw }
+  },
+
+  data() {
+    // Initialize some more reactive variables
+    return {
+      messageText: '',
+      editID: '',
+      editText: '',
+      recipient: ''
+    }
+  },
+
+  computed: {
+    messages() {
+      const messages = this.messagesRaw
+        // Filter the "raw" messages for data
+        // that is appropriate for our application
+        // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-note
+        .filter(m=>
+          // Does the message have a type property?
+          m.type         &&
+          // Is the value of that property 'Note'?
+          m.type=='Note' &&
+          // Does the message have a content property?
+          m.content      &&
+          // Is that property a string?
+          typeof m.content=='string') 
+        // Sort the messages according with the
+        // most recently created ones first
+        .sort((m1, m2)=> new Date(m2.published) - new Date(m1.published))
+        // Only show the 10 most recent ones
+        .slice(0,10)
+
+      // Do some more filtering for private messaging
+      return !this.privateMessaging? messages: messages.filter(m=>
+        // Is the message private?
+        m.bto &&
+        // Is the message only to one person?
+        m.bto.length == 1 &&
+        (
+          // Is the message to the recipient?
+          m.bto[0] == this.recipient ||
+          // Or is the message from the recipient?
+          m.actor == this.recipient
+        ))
+    },
+  },
+
+  methods: {
+    sendMessage() {
+      const message = {
+        type: 'Note',
+        content: this.messageText,
+      }
+
+      // The context field declares which
+      // channel(s) the object is posted in
+      // You can post in more than one if you want!
+      // The bto field makes messages private
+      if (this.privateMessaging) {
+        message.bto = [this.recipient]
+        message.context = [this.$gf.me, this.recipient]
+      } else {
+        message.context = [this.channel]
+      }
+
+      // Send!
+      this.$gf.post(message)
+    },
+
+    removeMessage(message) {
+      this.$gf.remove(message)
+    },
+
+    startEditMessage(message) {
+      // Mark which message we're editing
+      this.editID = message.id
+      // And copy over it's existing text
+      this.editText = message.content
+    },
+
+    saveEditMessage(message) {
+      // Save the text (which will automatically
+      // sync with the server)
+      message.content = this.editText
+      // And clear the edit mark
+      this.editID = ''
+    }
+  }
+}
+
+const Name = {
+  props: ['actor', 'editable'],
+
+  setup(props) {
+    // Get a collection of all objects associated with the actor
+    const { actor } = Vue.toRefs(props)
+    const $gf = Vue.inject('graffiti')
+    return $gf.useObjects([actor])
+  },
+
+  computed: {
+    profile() {
+      return this.objects
+        // Filter the raw objects for profile data
+        // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-profile
+        .filter(m=>
+          // Does the message have a type property?
+          m.type &&
+          // Is the value of that property 'Profile'?
+          m.type=='Profile' &&
+          // Does the message have a name property?
+          m.name &&
+          // Is that property a string?
+          typeof m.name=='string')
+        // Choose the most recent one or null if none exists
+        .reduce((prev, curr)=> !prev || curr.published > prev.published? curr : prev, null)
+    }
+  },
+
+  data() {
+    return {
+      editing: false,
+      editText: ''
+    }
+  },
+
+  methods: {
+    editName() {
+      this.editing = true
+      // If we already have a profile,
+      // initialize the edit text to our existing name
+      this.editText = this.profile? this.profile.name : this.editText
+    },
+
+    saveName() {
+      if (this.profile) {
+        // If we already have a profile, just change the name
+        // (this will sync automatically)
+        this.profile.name = this.editText
+      } else {
+        // Otherwise create a profile
+        this.$gf.post({
+          type: 'Profile',
+          name: this.editText
+        })
+      }
+
+      // Exit the editing state
+      this.editing = false
+    }
+  },
+
+  template: '#name'
+}
+
+app.components = { Name }
+Vue.createApp(app)
+   .use(GraffitiPlugin(Vue))
+   .mount('#app')
